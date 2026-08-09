92588815b1334d259c75a83ba618911c41ede2c7
L3o-ctrl <leo.tien.kahn@gmail.com>
2026-08-09 11:16:09 -0700

    added phase experiment A/D to change phase L/R arrows to change freq

---
+++ single wave.py
@@ -0,0 +1,79 @@
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
+import sounddevice as sd
+import numpy as np
+import keyboard as k
+from scipy.fft import fft, fftfreq
+BITRATE=96000
+duration=0.1
+running=True
+f=440
+period=1/f
+phase=0
+fig, ax = plt.subplots()
+
+
+def make_sine(f,duration,BITRATE,phase):
+    duration=round(duration*f)/f
+    samples = int(BITRATE * duration)
+
+    data = np.zeros((samples, 2),dtype='float32')
+    #the shape of the array has samples amount of rows
+    times = np.linspace(0, duration, samples)  # x axis
+    #the shape of times
+    data[:samples, 0] = np.sin(times * f * 2 * np.pi)
+    data[:samples, 1] = np.sin((times * f * 2 * np.pi)+phase)
+
+    print(f"  {f} Hz      ", end='', flush=True)
+    print(f"  {phase}/16                            ", end='\r', flush=True)
+    return data, times
+
+def up():
+    global f
+    f = f + 10
+
+def down():
+    global f
+    if f>70:
+        f=f-10
+
+def out():
+    global running
+    running=False
+
+def phase_up():
+    global phase,data,line2
+    if phase < (2 * np.pi):
+        phase += (np.pi*2)/48
+        data, times = make_sine(f, duration, BITRATE, phase)
+        line2.set_ydata(data[:times.shape[0], 1])
+        plt.draw()
+def phase_down():
+    global phase,data,line2
+    if 0 < phase:
+        phase -= (np.pi*2)/48
+        data, times = make_sine(f, duration, BITRATE,phase)
+        line2.set_ydata(data[:times.shape[0], 1])
+        plt.draw()
+def fft_process(data,samplerate):
+    n=data.shape[0]
+    yf = fft(data)
+    xf = fftfreq(n, 1 / samplerate)
+    #TODO:change pass into a return value
+    pass
+k.add_hotkey('right',up)
+k.add_hotkey('left',down)
+k.add_hotkey('q',out)
+k.add_hotkey('a',phase_up)
+k.add_hotkey('d',phase_down)
+data, times = make_sine(f, duration, BITRATE, phase)
+line,=ax.plot(times, data[:times.shape[0], 0])
+line2,=ax.plot(times, data[:times.shape[0], 1])
+plt.ion()
+plt.show()
+
+with sd.OutputStream(channels=2, samplerate=BITRATE) as stream:
+    while running:
+        data,t=make_sine(f,duration, BITRATE, phase)
+        stream.write(data)
+        plt.pause(0.001)
