5cce2581ea47704e3e6faffed849915553069a8e
L3o-ctrl <leo.tien.kahn@gmail.com>
2026-09-01 18:27:27 -0700

    The data shown is the raw data, not the fft data.

--- mic_fft.py
+++ mic_fft.py
@@ -3,10 +3,10 @@ from scipy.fft import fft, fftfreq
 import numpy as np
 import matplotlib.pyplot as plt
 fig2, ax2 = plt.subplots()
-samplerate=96000
-duration=0.2
+samplerate=96000 #samples/second (Hz=1/s)
+duration=0.01 # window time(s)
 channels=1
-sampInDur=int(samplerate*duration)
+sampInDur=int(samplerate*duration) #samples in duration
 def graph_update(yf):
     global fft_line
     fft_line.set_ydata(yf)
@@ -15,19 +15,19 @@ def graph_update(yf):
 def fft_process(data,samplerate):
     n=data.shape[0]
     yf = fft(data)
-    xf = fftfreq(n, 1 / samplerate)
+    xf = fftfreq(n, 1 / samplerate) #1/ samplerate=time for one sample
     yf=np.absolute(yf)
     return yf, xf
 
-data=np.zeros((sampInDur,channels))
-yf, xf=fft_process(data,samplerate)
-fft_line,=ax2.plot(xf, yf, 'c')
+data=np.zeros((sampInDur,channels)) #makes an array of length samples in duration
+yf, xf=fft_process(data,samplerate) # defining xf and yf
+fft_line,=ax2.plot(xf, yf, '.') #creating fft_line on the figure
 plt.ion()
 plt.show()
-with sd.InputStream(channels=channels, samplerate=samplerate, blocksize=sampInDur) as stream:
+with sd.InputStream(channels=channels, samplerate=samplerate, blocksize=sampInDur*10) as stream:
     while True:
         data,overflow=stream.read(sampInDur)
         yf,xf=fft_process(data, samplerate)
         graph_update(yf)
-        plt.pause(0.001)
+        plt.pause(0.1)
 
