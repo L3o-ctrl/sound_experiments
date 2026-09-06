6f10ea36b3d5468294c38e00fb4004b4f385538d
L3o-ctrl <leo.tien.kahn@gmail.com>
2026-09-06 12:58:55 -0700

    I added the Hz to note converter. Run it in a terminal for the print to work.

--- mic_fft.py
+++ mic_fft.py
@@ -1,13 +1,14 @@
 import sounddevice as sd
+import math
 from numpy.ma.mrecords import addfield
 from scipy.fft import fft, fftfreq
 import numpy as np
 import matplotlib.pyplot as plt
 fig2, ax2 = plt.subplots()
-samplerate=96000 #samples/second (Hz=1/s)
+samplerate=48000 #samples/second (Hz=1/s)
 # duration=0.01 # window time(s)
 channels=1
-sampInDur=2048#int(samplerate*duration) #samples in duration
+sampInDur=4096#int(samplerate*duration) #samples in duration
 def graph_update(yf):
     global fft_line
     fft_line.set_ydata(yf)
@@ -19,7 +20,21 @@ def fft_process(data,samplerate):
     xf = fftfreq(n, 1 / samplerate) #1/ samplerate=time for one sample
     yf=np.absolute(yf)
     return yf, xf
-
+def dom_freq(xf, yf):
+    max_amp = np.argmax(yf)
+    main_freq=xf[max_amp]
+    return main_freq
+def dom_note(freq):
+    if freq>0:
+        notes = ['A', 'A#', 'B', 'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#']
+        note_number = 12 * math.log2(freq / 440) + 49
+        note_number = round(note_number)
+        note = (note_number - 1) % len(notes)
+        note = notes[note]
+        octave = (note_number + 8) // len(notes)
+        return note, octave
+    else:
+        return None, None
 data=np.zeros((sampInDur,channels)) #makes an array of length samples in duration
 yf, xf=fft_process(data,samplerate) # defining xf and yf
 fft_line,=ax2.plot(xf, yf, 'r')#creating fft_line on the figure
@@ -34,6 +49,10 @@ with sd.InputStream(channels=channels, samplerate=samplerate, blocksize=sampInDu
         data = data[:,0]*window
         #print(window.shape)
         yf,xf=fft_process(data, samplerate)
+        main_freq=dom_freq(xf, yf)
+        note, octave=dom_note(main_freq)
+        #print(f"{main_freq} Hz                ",end="")
+        print(f"{main_freq} Hz {note} {octave}               ",end="\r")
         graph_update(yf)
         plt.pause(0.1)
 
